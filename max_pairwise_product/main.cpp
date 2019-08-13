d4dbf524fc522af50668c4143298d25ceeea0396
graveszhang <graves_zhang@sjtu.edu.cn>
2019-08-13 18:23:11 +0800

    pass

--- max_pairwise_product/main.cpp
+++ max_pairwise_product/main.cpp
@@ -6,9 +6,9 @@ int64_t  maxpair(const vector<int64_t>& numbers, int num){
    int64_t cand1 = 0, cand2 = 0;
     for (int i = 0; i < num; ++i) {
         if (numbers[i] > cand2)
-            cand2 = numbers[i];
+            cand2 = numbers[i]; // 防止最后一个数（第二大的数）小于cand1，就先给cand2
         if (numbers[i] > cand1){
-            cand2 = cand1;
+            cand2 = cand1; // cand1如果要变，就把之前cand1的值给cand2
             cand1 = numbers[i];
         }
     }
