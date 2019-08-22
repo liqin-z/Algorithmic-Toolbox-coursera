9de3c46116a318763b67147b6a32e087236acb5e
graveszhang <graves_zhang@sjtu.edu.cn>
2019-08-22 16:07:42 +0800

    pisano finished

--- codes/week2/pisano_loop/main.cpp
+++ codes/week2/pisano_loop/main.cpp
@@ -3,24 +3,23 @@
 
 long long fibo_module(long long n, long long m) {
     if (n <= 1) return n;
-    long long cycle = 0;
 
-    std::vector<long long> pisano(10000);
+    std::vector<long long> pisano(6 * m);
     pisano[0] = 0, pisano[1] = 1;
 
-    for (long long j = 2; j < n; ++j)
-        pisano[j] = (pisano[j - 1] + pisano[j - 2]) % m;
-
-    for (long long i = 2; i < n; ++i) {
-        if (pisano[i] == 0 && pisano[i + 1] == 1) {
-            cycle = i;
+    unsigned long long period = 2;
+    while (1) {
+        pisano[period] = (pisano[period - 1] + pisano[period - 2]) % m;
+        if (pisano[period - 1] == 0 && pisano[period] == 1) {
+            period--;
             break;
         }
+        period++;
     }
-    return pisano[n % cycle];
+    return pisano[n % period];
 }
 
-int main(){
+int main() {
     long long n, m;
     std::cin >> n >> m;
     std::cout << fibo_module(n, m) << "\n";
