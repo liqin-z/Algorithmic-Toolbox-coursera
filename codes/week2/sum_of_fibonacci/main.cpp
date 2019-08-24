edb7213ca93bac5c1a64047ee933e3b59fb5f327
graveszhang <graves_zhang@sjtu.edu.cn>
2019-08-24 18:51:29 +0800

    Naive sum

---
+++ codes/week2/sum_of_fibonacci/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+long long sum_fibo(int n){
+    long long prev = 0, curr = 1, last = 1;
+    for (int i = 2; i <= n; i++) {
+        long long temprev = prev;
+        prev = curr;
+        curr += temprev;
+//        cout << "curr: " << curr << " ";
+        last += curr;
+//        cout << "last: " << last << " ";
+    }
+    return last % 10; // 方法不够高效 和好像是错的
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << sum_fibo(n) << endl;
+    return 0;
+}
