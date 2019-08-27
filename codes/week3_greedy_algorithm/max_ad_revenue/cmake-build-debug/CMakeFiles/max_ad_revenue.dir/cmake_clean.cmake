78e6bdaf482e6004f87e1c639e6ec70199fde532
graveszhang <graves_zhang@sjtu.edu.cn>
2019-08-27 14:08:25 +0800

    bubble for max dot product

---
+++ codes/week3_greedy_algorithm/max_ad_revenue/cmake-build-debug/CMakeFiles/max_ad_revenue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/max_ad_revenue.dir/main.cpp.o"
+  "max_ad_revenue.pdb"
+  "max_ad_revenue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/max_ad_revenue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
