3651521109b0871d78d81704ab4600e91b027b5e
graveszhang <graves_zhang@sjtu.edu.cn>
2019-08-29 14:09:27 +0800

    re-upload

--- codes/week2_algorithm_warmup/2.1_fibonacci/fibonacci.cpp
+++ codes/week2_algorithm_warmup/2.1_fibonacci/fibonacci.cpp
@@ -3,24 +3,21 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    // 1 1 2 3 5 8 13 21 34 55
+    // 0 1 1 2 3 5 8 13 21 34 55
     int n;
     cin >> n;
-    int* fibo = new int[n];
-    fibo[0] = 1;
+    vector<int> fibo(n+1);
+    fibo[0] = 0;
     fibo[1] = 1;
 
-    for (int j = 2; j < n; j++)
+    for (int j = 2; j < n + 1; ++j)
         fibo[j] = fibo[j-1] + fibo[j-2];
 
-    if (n == 0)
-        cout << 1 << endl;
-    else
-        cout << fibo[n-1] << endl;
+    cout << fibo.at(n) << endl;
 
-    delete []fibo;
     return 0;
 }
