ec838a2c93ca6cdf6dfdf104f703f7507e335601
graveszhang <graves_zhang@sjtu.edu.cn>
2019-08-31 13:27:17 +0800

    solved

---
+++ codes/week3_greedy_algorithm/3.6_max_num_prizes/cmake-build-debug/CMakeFiles/3_6_max_num_prizes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3_6_max_num_prizes.dir/main.cpp.o"
+  "3_6_max_num_prizes.pdb"
+  "3_6_max_num_prizes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_6_max_num_prizes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
