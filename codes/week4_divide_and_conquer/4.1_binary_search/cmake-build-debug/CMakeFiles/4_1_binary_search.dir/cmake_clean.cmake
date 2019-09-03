e46a3ba3122342fe15710c4c378ca254844b366f
graveszhang <graves_zhang@sjtu.edu.cn>
2019-09-03 14:11:13 +0800

    no recursive needed

---
+++ codes/week4_divide_and_conquer/4.1_binary_search/cmake-build-debug/CMakeFiles/4_1_binary_search.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/4_1_binary_search.dir/binary_search.cpp.o"
+  "4_1_binary_search.pdb"
+  "4_1_binary_search"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_1_binary_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
